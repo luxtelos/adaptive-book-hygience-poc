712e8ee4b1e7b033ffe0123bb0ba86ec9ec0fdb8
RajviKamani1 <rajvik@zenithive.com>
2025-08-04 15:39:28 +0530

    add vite config and update the package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // This function checks if a module is from a node_modules directory.
+          // If it is, it assigns it to a chunk named 'vendor'.
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 });
